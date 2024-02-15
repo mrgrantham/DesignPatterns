f00e6b75b02a78814f5741c7640356c366978a05
James Grantham <james.grantham@gmail.com>
2024-02-15 15:43:49 -0800

    Interpreter Design Pattern

--- CppPatterns/Behavioral/Interpreter/src/InterpreterExample.h
+++ CppPatterns/Behavioral/Interpreter/src/InterpreterExample.h
@@ -1,87 +1,44 @@
-#include <fstream>
 #include <iostream>
-#include <memory>
-#include <string>
-#include <vector>
 
-class ConfigFile {
+class Expression {
 public:
-  virtual std::vector<std::string> getSettings() = 0;
-  virtual ~ConfigFile() = default;
+  virtual int evaluate() = 0;
 };
 
-class RealConfigFile : public ConfigFile {
-public:
-  explicit RealConfigFile(const std::string &filename) {
-    std::cout << "RealConfigFile created" << std::endl;
-
-    std::ifstream file(filename);
-    std::string line;
-
-    while (getline(file, line)) {
-      settings_.push_back(line);
-    }
-    std::cout << settings_.size() << " settings loaded" << std::endl;
-  }
-  std::vector<std::string> getSettings() { return settings_; }
-
-private:
-  std::vector<std::string> settings_;
-};
-
-// ConfigFile Proxy only implements the RealConfigFile the first time it is
-// accessed.
-class ConfigFileProxy : public ConfigFile {
-private:
-  std::string filename_;
-  std::unique_ptr<RealConfigFile> realConfigFile_;
+class OperationExpression : public Expression {
+  std::string operatorSymbol_;
+  Expression *leftHandSide_;
+  Expression *rightHandSide_;
 
 public:
-  explicit ConfigFileProxy(const std::string &filename)
-      : filename_(filename), realConfigFile_(nullptr) {
-    std::cout << "ConfigFileProxy created" << std::endl;
-  }
-  std::vector<std::string> getSettings() override {
-    if (!realConfigFile_) {
-      realConfigFile_ = std::make_unique<RealConfigFile>(filename_);
+  OperationExpression(const std::string &operatorSymbol, Expression *lhs,
+                      Expression *rhs)
+      : operatorSymbol_(operatorSymbol), leftHandSide_(lhs),
+        rightHandSide_(rhs) {}
+  int evaluate() override {
+    if (operatorSymbol_ == "+") {
+      return leftHandSide_->evaluate() + rightHandSide_->evaluate();
+    } else if (operatorSymbol_ == "-") {
+      return leftHandSide_->evaluate() - rightHandSide_->evaluate();
+
+    } else if (operatorSymbol_ == "/") {
+      return leftHandSide_->evaluate() / rightHandSide_->evaluate();
+
+    } else if (operatorSymbol_ == "*") {
+      return leftHandSide_->evaluate() * rightHandSide_->evaluate();
+
+    } else {
+      std::cout << "Unrecognized operator [" << operatorSymbol_ << "]"
+                << std::endl;
+      return 0;
     }
-    return realConfigFile_->getSettings();
   }
 };
 
-// Protective Proxy Example
-class Storage {
-  public:
-  virtual const std::string getContents()=0;
-  virtual ~Storage() = default;
+class NumberExpression : public Expression {
+  std::string number_;
 
-};
-
-class SecureStorage : public Storage {
-  public:
-  explicit SecureStorage(const std::string &data) : contents_(data) {
-    std::cout << "Creating secure storage" << std::endl;
-  }
-
-  const std::string getContents() {return contents_;}
-  private:
-  std::string contents_;
-};
-
-class SecureStorageProxy : public Storage {
-  private: 
-  const std::string passcode_ = "1234";
-  std::unique_ptr<SecureStorage> secureStorage_;
-  public:
-  explicit SecureStorageProxy(const std::string &passcode, const std::string &data) : secureStorage_(passcode_ == passcode? new SecureStorage(data) : nullptr) {
-
-  }
-
-  const std::string getContents() {
-    if (secureStorage_) {
-      return secureStorage_->getContents();
-    }
-    return "invalid passcode";
-  }
-
+public:
+  NumberExpression(const std::string &number) : number_(number) {}
+  int evaluate() override { return std::stoi(number_); }
 };
--- CppPatterns/Behavioral/Interpreter/src/main.cpp
+++ CppPatterns/Behavioral/Interpreter/src/main.cpp
@@ -7,5 +7,19 @@ int main(int argc, char **argv) {
 
   std::cout << "Interpreter Pattern Example" << std::endl;
 
+  NumberExpression *five = new NumberExpression("5");
+  NumberExpression *seven = new NumberExpression("7");
+
+  OperationExpression *fivePlusSeven =
+      new OperationExpression("+", five, seven);
+
+  std::cout << "Evaluate to " << fivePlusSeven->evaluate() << std::endl;
+
+  NumberExpression *thirteen = new NumberExpression("13");
+
+  OperationExpression *complexOp =
+      new OperationExpression("-", thirteen, fivePlusSeven);
+  std::cout << "Evaluate to " << complexOp->evaluate() << std::endl;
+
   return EXIT_SUCCESS;
 }
